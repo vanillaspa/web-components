d160083f8ab0810b552a2788025a1e20709bad82
J4H20M3 <rome@live.com>
2026-02-22 00:53:01 +0100

    refactored: removed generate.cjs in favor of import.meta.glob

--- index.js
+++ index.js
@@ -9,70 +9,74 @@ window.getShadowDocument = function magic(hostDataIDs) {
         for (let hostDataID of hostDataIDs) {
             let found = shadowDOM.querySelector('[data-id="' + hostDataID + '"]');
             if (found) { shadowDOM = found.shadowRoot; } else {
-                console.error({hostDataID});
-                return null; }
+                console.error({ hostDataID });
+                return null;
+            }
         }
         return shadowDOM;
     }
     return null;
 }
-for (const filePath of htmlFiles) {
+
+function getComponentName(filePath) {
     const fileName = filePath.split("/").pop();
     const dashSplit = fileName.split('-');
     const prefix = dashSplit[0];
     const dotSplit = dashSplit[1].split('.'); // TODO accept three-fold-components
     const suffix = dotSplit[0];
-    fetch(filePath)
-    .then(file => file.text())
-    .then(html => {
-        const fragment = document.createRange().createContextualFragment(html);
-        const scriptFragment = fragment.querySelector("script");
-        const styleFragment = fragment.querySelector("style");
-        const templateFragment = fragment.querySelector("template");
-        customElements.define(`${prefix}-${suffix}`, class extends HTMLElement {
-            constructor() {
-                super();
-                const shadowRoot = this.attachShadow({ mode: "open" });
+    return `${prefix}-${suffix}`;
+}
+
+for (let filePath of Object.keys(htmlFiles)) {
+    let html = htmlFiles[filePath];
+    let componentName = getComponentName(filePath);
+    const fragment = document.createRange().createContextualFragment(html);
+    const scriptFragment = fragment.querySelector("script");
+    const styleFragment = fragment.querySelector("style");
+    const templateFragment = fragment.querySelector("template");
+    customElements.define(componentName, class extends HTMLElement {
+        constructor() {
+            super();
+            const shadowRoot = this.attachShadow({ mode: "open" });
+        }
+        connectedCallback() {
+            this.hostDataIDs = []; // used to find the nested shadowDocument
+            this.dataset.id = Math.random().toString(16).substring(2, 8); // should suffice
+            let hostElement = this;
+            while (hostElement && hostElement.dataset.id) { // +3 get parent.host data-id 's
+                this.hostDataIDs.push(hostElement.dataset.id);
+                hostElement = hostElement.getRootNode().host;
             }
-            connectedCallback() {
-                this.hostDataIDs = []; // used to find the nested shadowDocument
-                this.dataset.id = Math.random().toString(16).substring(2, 8); // should suffice
-                let hostElement = this;
-                while (hostElement && hostElement.dataset.id) { // +3 get parent.host data-id 's
-                    this.hostDataIDs.push(hostElement.dataset.id);
-                    hostElement = hostElement.getRootNode().host;
-                }
-                this.#render();
-            }
-            #render() {
-                this.shadowRoot.replaceChildren();
-                this.#appendTemplate();
-                this.#appendStyle();
-                this.#appendScript();
-            }
-            #appendScript() {
-                if (scriptFragment) {
-                    const scriptElement = document.createElement("script");
-                    scriptElement.setAttribute("type", "module");
-                    scriptElement.textContent = `
+            this.#render();
+        }
+        #render() {
+            this.shadowRoot.replaceChildren();
+            this.#appendTemplate();
+            this.#appendStyle();
+            this.#appendScript();
+        }
+        #appendScript() {
+            if (scriptFragment) {
+                const scriptElement = document.createElement("script");
+                scriptElement.setAttribute("type", "module");
+                scriptElement.textContent = `
 const shadowDocument = getShadowDocument('${this.hostDataIDs.toReversed().toString()}');
 ${scriptFragment ? scriptFragment.textContent : ''}
 `;
-                    this.shadowRoot.appendChild(scriptElement);
-                }
+                this.shadowRoot.appendChild(scriptElement);
             }
-            #appendStyle() {
-                if (styleFragment) {
-                    let clonedStyle = styleFragment.cloneNode(true);
-                    this.shadowRoot.appendChild(clonedStyle);
-                }
+        }
+        #appendStyle() {
+            if (styleFragment) {
+                let clonedStyle = styleFragment.cloneNode(true);
+                this.shadowRoot.appendChild(clonedStyle);
             }
-            #appendTemplate() {
-                if (templateFragment) {
-                    const clonedFragment = templateFragment.content.cloneNode(true);
-                    this.shadowRoot.appendChild(clonedFragment);
-                }
+        }
+        #appendTemplate() {
+            if (templateFragment) {
+                const clonedFragment = templateFragment.content.cloneNode(true);
+                this.shadowRoot.appendChild(clonedFragment);
             }
-        });
+        }
     });
 }
